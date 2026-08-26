206a0ad83954e0cc529005157ffe115ec54f20a7
YI YONGSINH <sinh3784@gmail.com>
2026-08-27 02:20:34 +0700

    new thing(add cloudinary)

--- src/main/java/com/example/spring_boot_project_api/config/OpenApiConfig.java
+++ src/main/java/com/example/spring_boot_project_api/config/OpenApiConfig.java
@@ -1,8 +1,9 @@
 package com.example.spring_boot_project_api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,15 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info()
-                        .title("Tourism Project API")
-                        .version("1.0.0")
-                        .description("API Documentation for Tourism Web Application Backend")
-                        .contact(new Contact()
-                                .name("Development Team")
-                                .email("tourism@admin.com")));
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
